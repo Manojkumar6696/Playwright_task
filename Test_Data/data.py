507f1b420d64cbf6ec7753401f3f56b6b9e0691a
Manojkumar <140330455+Manojkumar6696@users.noreply.github.com>
2025-05-16 19:51:26 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,9 @@
+class Data:
+    Guvi_url = "https://v2.zenclass.in/login"
+    dashboard_url = "https://v2.zenclass.in/dashboard"
+    # valid credentials
+    username = "manojsekar.6696@gmail.com"
+    password = "Lucky@96"
+    # invalid credentials
+    invalid_username = "Invaliduser"
+    invalid_password = "Invalidpass"
